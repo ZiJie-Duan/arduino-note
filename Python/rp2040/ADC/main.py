2083e62d2c7af0c821424dca02ac98f6222757ac
lucycore <32613263+lucycore@users.noreply.github.com>
2022-12-27 01:37:35 +0800

    update something not important

--- Python/rp2040/ADC/main.py
+++ Python/rp2040/ADC/main.py
@@ -1,9 +1,22 @@
-import ssd1306 as SSD1306_I2C
- 
-#lcd.imit_i2c(SCL引脚,SDA引脚,宽,长,i2c控制器编号官方图给出)
- 
-lcd.init_i2c(5,4, 128, 64, 0)
-lcd.text('font8x8', 0, 0, 8)
-lcd.text('font16x16', 0, 20, 16)
-lcd.text('font24x24', 0, 40, 24)
-lcd.show()
+import ssd1327
+
+# using Software I2C
+from machine import SoftI2C, Pin
+i2c = SoftI2C(sda=Pin(21), scl=Pin(22)) # TinyPICO
+# i2c = SoftI2C(sda=Pin(0), scl=Pin(1)) # Raspberry Pi Pico
+# i2c = SoftI2C(sda=Pin(4), scl=Pin(5)) # WeMos D1 Mini
+
+# or using Hardware I2C
+from machine import I2C, Pin
+i2c = I2C(0) # TinyPICO sda=19, scl=18
+
+display = ssd1327.SEEED_OLED_96X96(i2c)  # Grove OLED Display
+# display = ssd1327.SSD1327_I2C(128, 128, i2c)  # WaveShare, Zio Qwiic
+
+display.text('Hello World', 0, 0, 255)
+display.show()
+
+display.fill(0)
+for y in range(0,12):
+    display.text('Hello World', 0, y * 8, 15 - y)
+display.show()
